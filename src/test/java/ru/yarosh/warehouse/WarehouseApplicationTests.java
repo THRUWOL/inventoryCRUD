133e5f5b5c9437e896a93f2893a1dcdd2dda7a28
NikitaYarosh <yarosh.nv@yandex.ru>
2024-02-24 12:42:31 +0300

    feature: настроил maven зависимости, добавил конфиг для подключения к БД

---
+++ src/test/java/ru/yarosh/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yarosh.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
