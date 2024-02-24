02cab40dd2142ea20a60ffb7e39badb82e9b169c
NikitaYarosh <yarosh.nv@yandex.ru>
2024-02-24 13:28:22 +0300

    feature: Добавил валидацию для полей сущности товара

--- src/main/java/ru/yarosh/warehouse/model/Product.java
+++ src/main/java/ru/yarosh/warehouse/model/Product.java
@@ -3,6 +3,8 @@ package ru.yarosh.warehouse.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -17,13 +19,36 @@ public class Product {
     @GeneratedValue
     private UUID id;
 
+    @NotBlank(message = "Name is required")
+    @NotNull
     private String name;
+
+    @NotBlank(message = "Article is required")
+    @NotNull
     private String article;
+
+    @NotBlank(message = "Description is required")
+    @NotNull
     private String description;
+
+    @NotBlank(message = "Category is required")
+    @NotNull
     private String category;
+
+    @NotBlank(message = "Price is required")
+    @NotNull
     private double price;
+
+    @NotBlank(message = "Quantity is required")
+    @NotNull
     private int quantity;
+
+    @NotBlank(message = "LastQuantityChange is required")
+    @NotNull
     private LocalDateTime lastQuantityChange;
+
+    @NotBlank(message = "CreationDate is required")
+    @NotNull
     private LocalDateTime creationDate;
 
 }
