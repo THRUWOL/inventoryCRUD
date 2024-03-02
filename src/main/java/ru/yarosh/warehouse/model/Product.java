300bcb2fdabf225590cf7a2a59a1b50e00bfeff4
NikitaYarosh <yarosh.nv@yandex.ru>
2024-03-02 15:53:33 +0300

    feature: добавил тесты

--- src/main/java/ru/yarosh/warehouse/model/Product.java
+++ src/main/java/ru/yarosh/warehouse/model/Product.java
@@ -5,13 +5,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
     @Id
